4549cc3aca3952c6da78c267063bdbebd78dcad2
Guilherme Peixoto <gpp@cin.ufpe.br>
2015-09-25 19:19:47 -0300

    stem and stop (default)

--- app/models/document.rb
+++ app/models/document.rb
@@ -3,5 +3,36 @@ require 'elasticsearch/model'
 class Document < ActiveRecord::Base
   include Elasticsearch::Model
   include Elasticsearch::Model::Callbacks
+
+  settings index: { number_of_shards: 1 } do
+    mappings dynamic: 'false' do
+      indexes :title, analyzer: 'english', index_options: 'offsets'
+      indexes :text, analyzer: 'english'
+    end
+  end
+
+  def self.search(query)
+    __elasticsearch__.search(
+      {
+        query: {
+          multi_match: {
+          query: query,
+          fields: ['title^10', 'text']
+          }
+        }
+      }
+    )
+  end
+
 end
+
+# Delete the previous documents index in Elasticsearch
+Document.__elasticsearch__.client.indices.delete index: Document.index_name rescue nil
+
+# Create the new index with the new mapping
+Document.__elasticsearch__.client.indices.create \
+  index: Document.index_name,
+  body: { settings: Document.settings.to_hash, mappings: Document.mappings.to_hash }
+
+# Index all document records from the DB to Elasticsearch
 Document.import
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,10 +5,12 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+# require 'elasticsearch/client'
 
 directory = './20_newsgroups_crypt_space-sample10/sci.crypt/'
 Dir.foreach(directory) do |file_name|
   next if file_name == '.' or file_name == '..'
   data = File.read(directory + file_name)
   Document.create(name: file_name, text: data)
+  # client.indices.create(name: file_name, text: data)
 end  
