3d72dc37f21e9b0152d1a060e86db5bdfd8c8ea4
Mateus <mmfrb@cin.ufpe.br>
2015-09-26 18:00:44 -0300

    adicionando os documentos de space na base

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,10 +7,13 @@
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 # require 'elasticsearch/client'
 
-directory = './20_newsgroups_crypt_space-sample10/sci.crypt/'
-Dir.foreach(directory) do |file_name|
-  next if file_name == '.' or file_name == '..'
-  data = File.read(directory + file_name)
-  Document.create(name: file_name, text: data)
-  # client.indices.create(name: file_name, text: data)
+read_files = Proc.new do |directory|
+  Dir.foreach(directory) do |file_name|
+    next if file_name == '.' or file_name == '..'
+    data = File.read(directory + file_name)
+    Document.create(name: file_name, text: data)
+  end
 end  
+
+read_files.call './20_newsgroups_crypt_space-sample10/sci.crypt/'
+read_files.call './20_newsgroups_crypt_space-sample10/sci.space/'
