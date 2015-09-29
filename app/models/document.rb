e549075da2795508415732b29176242633061e70
Guilherme Peixoto <gpp@cin.ufpe.br>
2015-09-29 00:54:36 -0300

    tag in html for doc id extraction and stem+no stop analyzer

--- app/models/document.rb
+++ app/models/document.rb
@@ -4,20 +4,39 @@ class Document < ActiveRecord::Base
   include Elasticsearch::Model
   include Elasticsearch::Model::Callbacks
 
-  settings index: { number_of_shards: 1 } do
+  settings index: {
+  "number_of_shards": 1,
+  "number_of_replicas": 0,
+  "analysis": {
+    "filter": {
+      "english_stemmer": {
+        "type":       "stemmer",
+        "language":   "english"
+      }
+    },
+    "analyzer": {
+      "eng": {
+        "tokenizer":  "standard",
+        "filter": ["lowercase","english_stemmer"]
+      }
+    }
+  }
+} do
     mappings dynamic: 'false' do
-      indexes :title, analyzer: 'english', index_options: 'offsets'
-      indexes :text, analyzer: 'english'
+      indexes :title, analyzer: 'eng', index_options: 'offsets'
+      indexes :text, analyzer: 'eng'
     end
   end
 
   def self.search(query)
     __elasticsearch__.search(
       {
+        size: 50,
+        min_score: 0.01 ,
         query: {
           multi_match: {
             query: query,
-            fields: ['title^10', 'text']
+            fields: ['title', 'text']
           }
         },
         highlight: {
